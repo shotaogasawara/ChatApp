1ce7cc20cd90b8658c602f1c1cf9fe21cc3eeebf
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-03-31 20:40:46 +0900

    Commit for flux_tutorial 4

--- app/assets/javascripts/src/stores/messages.js
+++ app/assets/javascripts/src/stores/messages.js
@@ -106,6 +106,7 @@ MessagesStore.dispatchToken = Dispatcher.register(payload => {
         timestamp: action.timestamp,
         from: UserStore.user.id,
       })
+      messages[userID].lastAccess.currentUser = +new Date()
       MessagesStore.emitChange()
       break
   }
