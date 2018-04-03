f63c1d07c4a03fe85744be1edf3e5168d78777c6
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-03 16:09:02 +0900

    change page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   namespace :api, { format: 'json' } do
     resources :messages
   end
 
+  resources :users, :only => [:index, :show]
   resources :messages
   root to: 'messages#index'
 
