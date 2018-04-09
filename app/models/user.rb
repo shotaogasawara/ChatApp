6ac0644e96fce6fb4d72c4845a6e551fa2b7ec16
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-09 16:17:19 +0900

    relate user and friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ActiveRecord::Base
   # 友達関係
   has_many :friendships_of_from_user, :class_name => 'Friendship', :foreign_key => 'from_user_id', :dependent => :destroy
   has_many :friendships_of_to_user, :class_name => 'Friendship', :foreign_key => 'to_user_id', :dependent => :destroy
-
+  has_many :friends_of_from_user, :through => :friendships_of_from_user, :source => 'to_user'
+  has_many :friends_of_to_user, :through => :friendships_of_to_user, :source => 'from_user'
+  def friends # user.friendsとしてuserのすべての友人にアクセス可能ｇ
+    friends_of_from_user + friends_of_to_user
+  end
 end
