556a7b741ac3aedbba36406f03285b9a3ebd0a2c
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-09 16:21:55 +0900

    define friends method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :friendships_of_to_user, :class_name => 'Friendship', :foreign_key => 'to_user_id', :dependent => :destroy
   has_many :friends_of_from_user, :through => :friendships_of_from_user, :source => 'to_user'
   has_many :friends_of_to_user, :through => :friendships_of_to_user, :source => 'from_user'
-  def friends # user.friendsとしてuserのすべての友人にアクセス可能ｇ
+  def friends # user.friendsとしてuserのすべての友人にアクセス可能
     friends_of_from_user + friends_of_to_user
   end
 end
