1c0a930e4ac39c7e602127f91acc95a91424d54f
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-09 15:45:57 +0900

    add profile picture column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402083736) do
+ActiveRecord::Schema.define(version: 20180409064452) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "message"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20180402083736) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "name"
+    t.string   "profilePicture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
