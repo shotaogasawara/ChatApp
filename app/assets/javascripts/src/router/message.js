c17ffe5cb7adbfb528f7f185394756f210adb8f3
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-12 18:20:50 +0900

    refactor file name

--- app/assets/javascripts/src/router/message.js
+++ app/assets/javascripts/src/router/message.js
@@ -1,7 +1,7 @@
 import ReactDecorator from '../base/react_decorator'
 import BaseRouter from '../base/router'
 import App from '../components/messages/app'
-import GetCurrentUserAction from '../actions/getCurrentUser'
+import GetCurrentUserAction from '../actions/currentUser'
 import GetSearchFriendAction from '../actions/searchFriend'
 
 export default class MessageRouter extends BaseRouter {
