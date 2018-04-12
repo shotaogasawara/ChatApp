e666391c555890d71d9bf1e82e0ba2d197d27506
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-12 13:42:05 +0900

    display chats

--- app/assets/javascripts/src/components/messages/messagesBox.js
+++ app/assets/javascripts/src/components/messages/messagesBox.js
@@ -19,7 +19,7 @@ class MessagesBox extends React.Component {
   getStateFromStore() {
     // return MessagesStore.getMessages() // ここでstoreから全メッセージを取得する
     return {
-      messages: MessagesStore.getMessages()
+      messages: MessagesStore.getMessages(),
     }
   }
 
@@ -37,63 +37,32 @@ class MessagesBox extends React.Component {
 
   //
   render() {
-    var allMessages = this.state.messages
-    allMessages.forEach((message) => {
-      console.log(message.content) // メッセージ取得はできていることを確認
+    const {messages} = this.state
+    const chatsList = messages.map(message => {
+      if (message.sender_id === this.props.user.id) {
+        return (
+          <li className='message-box__item message-box__item--from-current'>
+            <div className='message-box__item__contents message-box__item--from-current'>
+            {message.content}
+            </div>
+          </li>
+        )
+      } else {
+        return (
+          <li className='message-box__item'>
+            <div className='message-box__item__contents'>
+              {message.content}
+            </div>
+          </li>
+        )
+      }
     })
-
-    // var currentUserID = this.props.user.id
-    // // console.log(currentUserID)
-    // var messagesLength = Object.keys(allMessages).length
-    // // console.log(messagesLength)
-    // var messages = Object.keys(allMessages).map((message) => {
-    //   console.log(message.content)
-    //   return (
-    //     <li>
-    //       {message.content}
-    //     </li>
-    //   )
-    // })
-   // console.log(messages)
-
-
-    // const messagesLength = this.state.messages.length
-    // const currentUserID = this.props.user.id
-    // const messages = this.state.messages.map((message, index) => { // stateからmessageを取り出す
-    //   const messageClasses = classNames({
-    //     'message-box__item': true,
-    //     'message-box__item--from-current': message.from === currentUserID,
-    //     'clear': true,
-    //   })
-    //
-    //   return (
-    //     <li key={message.timestamp + '-' + message.from} className={messageClasses}>
-    //       <div className='message-box__item__contents'>
-    //         {message.contents}
-    //       </div>
-    //     </li>
-    //   )
-    // })
-    //
-    // const lastMessage = this.state.messages[messagesLength - 1]
-    //
-    // if (lastMessage.from === currentUserID) {
-    //   if (this.state.lastAccess.recipient >= lastMessage.timestamp) {
-    //     const date = Utils.getShortDate(lastMessage.timestamp)
-    //     messages.push(
-    //       <li key='read' className='message-box__item message-box__item--read'>
-    //         <div className='message-box__item__contents'>
-    //           Read {date}
-    //         </div>
-    //       </li>
-    //     )
-    //   }
-    // }
     return (
       <div className='message-box'>
         <ul className='message-box__list'>
+          {chatsList}
         </ul>
-        <ReplyBox />,
+        <ReplyBox/>,
       </div>
     )
   }
