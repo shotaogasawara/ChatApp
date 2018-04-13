32a222133990de0c9d0641b89a82218583ac823d
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-04-13 19:59:29 +0900

    refresh db

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.create!(name: "shota",
              password_confirmation: "foobar"
 )
 
-99.times do |n|
+10.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@prog-8.com"
   password = "password"
